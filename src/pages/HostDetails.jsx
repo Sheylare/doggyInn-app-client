0b836f0fd32a0bb039200e9137125f535ecc7daf
Marcos <mmarkito708@gmail.com>
2024-07-22 15:19:48 +0200

    listado de host, y detalles de cada host

--- src/pages/HostDetails.jsx
+++ src/pages/HostDetails.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useParams} from "react-router-dom"
+import axios from "axios"
 
 function HostDetails() {
+
+  const params = useParams()
+
+  const [host, setHost] = useState(null)
+
+  useEffect(() => {
+
+    getHost()
+
+  },[])
+
+  const getHost = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/hosts/${params.hostId}`)
+      console.log(response.data)
+      setHost(response.data)
+    } catch (error) {
+      
+    }
+
+  }
+
+  if (host === null) {
+    return <h2>... Buscando Host</h2>
+  }
+
   return (
-    <div>HostDetails</div>
+    <div>
+
+      <h3>Detalles del Host Seleccionado</h3>
+
+      <p>{host.name}</p>
+      <p>{host.description}</p>
+      <p>{host.city}</p>
+      <p>{host.adress}</p>
+      <p>{host.email}</p>
+      <p>{host.mobile}</p>
+      <p>{host.price}</p>
+
+      <button>Reservar</button>
+
+
+    </div>
   )
 }
 
