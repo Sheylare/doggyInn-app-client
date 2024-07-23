ca15a159792e55b144eb632ff9e9ae2b98db3f0c
Marcos <mmarkito708@gmail.com>
2024-07-23 17:27:47 +0200

    ultimos cambios

---
+++ src/components/locationHost.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+function LocationMarker({ onLocationSelect }) {
+  const [position, setPosition] = useState(null);
+
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+      onLocationSelect(e.latlng);
+    },
+  });
+  return position === null ? null : <Marker position={position} />;
+}
+function LocationPicker({ onLocationChange }) {
+  const defaultPosition = [40.463667, -3.74922];
+  const zoom = 13;
+  return (
+    <MapContainer
+      center={defaultPosition}
+      zoom={zoom}
+      style={{ height: "400px", width: "100%" }}
+    >
+      {" "}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />{" "}
+      <LocationMarker onLocationSelect={onLocationChange} />{" "}
+    </MapContainer>
+  );
+}
+export default LocationPicker;
--- src/pages/HostRegistration.jsx
+++ src/pages/HostRegistration.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import LocationPicker from "../components/locationHost";
 
 function HostRegistration() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [address, setAddress] = useState([0, 0]);
+  const [description, setDescription] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleLocationChange = (location) => {
+    setAddress([location.lat, location.lng]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newHost = {
+      name: name,
+      city: city,
+      address: address,
+      description: description,
+      email: email,
+      phone: phone,
+      price: price,
+    };
+
+    try {
+      await axios.post(`${import.meta.env.VITE_SERVER_URL}/hosts`, newHost);
+
+      navigate("/hosts");
+    } catch (error) {}
+  };
+
   return (
-    <div>HostRegistration</div>
-  )
+    <div>
+      <h3>Add Host</h3>
+
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>City:</label>
+        <input
+          type="text"
+          name="city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <label>Address (Latitud, Longiyud):</label>{" "}
+        <input
+          type="text"
+          name="address"
+          value={address.join(",")}
+          onChange={(e) => setAddress(e.target.value)}
+          readOnly
+        />{" "}
+        
+        <div>
+          <label>Select Location:</label>{" "}
+          <LocationPicker onLocationChange={handleLocationChange} />
+        </div>
+
+        <label>Description:</label>
+        <textarea
+          type="text"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label>Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Phone:</label>
+        <input
+          type="text"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <label>Price:</label>
+        <input
+          type="text"
+          name="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default HostRegistration
+export default HostRegistration;
